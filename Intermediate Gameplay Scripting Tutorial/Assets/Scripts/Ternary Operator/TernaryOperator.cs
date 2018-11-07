168a27ce2d9294c4b4ca76c569b33add33f42021
Twilit <kinlongsha@hotmail.co.uk>
2018-11-07 11:21:22 +0000

    Ternary Operator
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Ternary Operator/TernaryOperator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TernaryOperator : MonoBehaviour
+{
+
+	void Start ()
+    {
+        int health = 10;
+
+        string message;
+
+        message = health > 0 ? "Player is Alive" : health == 0 ? "Player is Barely Alive" : "Player is Dead";
+	}
+
+}
