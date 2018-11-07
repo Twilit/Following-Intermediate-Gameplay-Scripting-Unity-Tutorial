9f6213bd630c839d29d1ce7393171eecc5b77a70
Twilit <kinlongsha@hotmail.co.uk>
2018-11-07 11:43:37 +0000

    Method Overloading
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Method Overloading/MOSomeClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOSomeClass
+{
+
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public string Add(string string1, string string2)
+    {
+        return string1 + string2;
+    }
+
+}
---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Method Overloading/MOSomeOtherClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOSomeOtherClass : MonoBehaviour
+{
+
+	void Start ()
+    {
+		MOSomeClass myClass = new MOSomeClass():
+
+        myClass.Add(1, 2);
+        myClass.Add("Hello ", "World");
+	}
+
+}
