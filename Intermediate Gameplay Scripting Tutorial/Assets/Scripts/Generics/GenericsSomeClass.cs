c0892d57dd2034e7f63e2813605bd4ffe4dab3d7
Twilit <kinlongsha@hotmail.co.uk>
2018-11-07 11:54:25 +0000

    Generics
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Generics/GenericsSomeClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericsSomeClass
+{
+    public T GenericMethod<T>(T param)
+    {
+        return param;
+    }
+}
---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Generics/GenericsSomeOtherClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericsSomeOtherClass : MonoBehaviour
+{
+
+	void Start ()
+    {
+        GenericsSomeClass myClass = new GenericsSomeClass();
+
+        myClass.GenericMethod<int>(5);
+	}
+
+}
