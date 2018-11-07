6a9b7761d2cd390d50b784592aeff18e3b344ed9
Twilit <kinlongsha@hotmail.co.uk>
2018-11-07 11:08:09 +0000

    Properties
    finished

---
+++ Intermediate Gameplay Scripting/Properties/Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+	void Start ()
+    {
+        Player myPlayer = new Player();
+
+        myPlayer.Exp = 5;
+
+        int x = myPlayer.Exp;
+	}
+}
---
+++ Intermediate Gameplay Scripting/Properties/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private int exp;
+
+    public int Exp
+    {
+        get
+        {
+            return exp;
+        }
+
+        set
+        {
+            exp = value;
+        }
+    }
+
+    public int Level
+    {
+        get
+        {
+            return exp / 1000;
+        }
+
+        set
+        {
+            exp = value * 1000;
+        }
+    }
+}
