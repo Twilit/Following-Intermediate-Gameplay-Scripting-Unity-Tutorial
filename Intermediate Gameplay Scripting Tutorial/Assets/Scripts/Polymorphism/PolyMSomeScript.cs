170e3dc260c2006d86a4a8aa8a9893ad687b89f7
Twilit <kinlongsha@hotmail.co.uk>
2018-11-09 13:48:39 +0000

    Polymorphism
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Polymorphism/PolyMSomeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolyMSomeScript : MonoBehaviour
+{
+
+	void Start ()
+    {
+        PolyMParentClass myClass = new PolyMChildClass();
+
+        myClass.ParentMethod();
+
+        ((PolyMChildClass)myClass).ChildMethod();
+
+        PolyMChildClass myChild = (PolyMChildClass)myClass;
+
+        myChild.ChildMethod();
+	}
+
+}
