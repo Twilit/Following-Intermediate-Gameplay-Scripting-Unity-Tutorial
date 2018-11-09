49d004f401696e9c3b88b586821d1cafe9c65e91
Twilit <kinlongsha@hotmail.co.uk>
2018-11-09 13:32:50 +0000

    Inheritance
    Finished

--- Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Inheritance/InheritanceApple.cs
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Inheritance/InheritanceApple.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InheritanceApple : MonoBehaviour {
+public class InheritanceApple : InheritanceFruit
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public InheritanceApple()
+    {
+        //Constructor code
+    }
+
 }
