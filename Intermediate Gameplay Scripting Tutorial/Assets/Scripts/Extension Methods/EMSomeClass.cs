5cbffcc04047525053031e7e6b7ffc7cbf798aab
Twilit <37117133+Twilit@users.noreply.github.com>
2018-11-11 22:23:40 +0000

    Extensions
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Extension Methods/EMSomeClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMSomeClass : MonoBehaviour
+{
+
+	void Start ()
+    {
+        transform.ResetTransformation();
+	}
+
+}
