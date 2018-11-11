302516913e1f1230ca968c6900d2ce8772a8bd52
Twilit <37117133+Twilit@users.noreply.github.com>
2018-11-11 22:30:49 +0000

    Namespaces
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Namespaces/NamespacesSomeClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SampleNamespace
+{
+    public class NamespacesSomeClass : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+    }
+}
---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Namespaces/NamespacesSomeOtherClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using SampleNamespace;
+
+namespace SampleNamespace
+{
+    public class NamespacesSomeOtherClass : MonoBehaviour
+    {
+
+        void Start()
+        {
+            //SampleNamespace.NamespacesSomeClass myClass = new SampleNamespace.NamespacesSomeClass();
+
+            NamespacesSomeClass myClass = new NamespacesSomeClass();
+        }
+
+    }
+}
