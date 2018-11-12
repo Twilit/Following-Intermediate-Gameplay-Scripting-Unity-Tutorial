4097b8d0656fb9b02e5810443293d69c03697741
Twilit <37117133+Twilit@users.noreply.github.com>
2018-11-12 01:43:51 +0000

    Lists & DIctionaries
    at 3:37

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Lists and Dictionaries/LDBadGuy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LDBadGuy : IComparable<LDBadGuy>
+{
+    public string name;
+    public int power;
+
+    public LDBadGuy(string newName, int newPower)
+    {
+        name = newName;
+        power = newPower;
+    }
+
+    public int CompareTo(LDBadGuy other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        return power - other.power;
+    }
+}
---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Lists and Dictionaries/LDSomeClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LDSomeClass : MonoBehaviour
+{
+
+
+	void Start ()
+	{
+        List<LDBadGuy> badGuys = new List<LDBadGuy>();
+
+        badGuys.Add(new LDBadGuy("Harvey", 50));
+        badGuys.Add(new LDBadGuy("Magneto", 100));
+        badGuys.Add(new LDBadGuy("Pip", 5));
+
+        badGuys.Sort();
+	}
+}
