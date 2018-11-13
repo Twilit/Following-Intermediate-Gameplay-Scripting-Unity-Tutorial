e27cc9fe7dcc67fefbc612e63d286263d4bddb17
Twilit <kinlongsha@hotmail.co.uk>
2018-11-13 11:34:54 +0000

    Delegates
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Delegates/MulticastCast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastCast : MonoBehaviour
+{
+    delegate void MultiDelegate();
+    MultiDelegate myMultiDelegate;
+
+	void Start ()
+    {
+        myMultiDelegate += PowerUp;
+        myMultiDelegate += TurnRed;
+
+        if (myMultiDelegate != null)
+        {
+            myMultiDelegate();
+        }
+	}
+
+	void PowerUp ()
+    {
+        print("Orb is powering up!");
+	}
+
+    void TurnRed()
+    {
+        //renderer.material.color = Color.red;
+    }
+}
