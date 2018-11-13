90785c739d138d8198226f0ea4ae12286be998c2
Twilit <kinlongsha@hotmail.co.uk>
2018-11-13 12:10:53 +0000

    Events
    at 5:21

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Events/TurnColour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnColour : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.OnClicked += TurnColourMethod;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnClicked -= TurnColourMethod;
+    }
+
+    void TurnColourMethod()
+    {
+        Color col = new Color(Random.value, Random.value, Random.value);
+        //renderer.material.color = col;
+    }
+}
