8d0ff3aafbcb9ec497e77ac43b7ed906162e96e1
Twilit <kinlongsha@hotmail.co.uk>
2018-11-13 10:56:40 +0000

    Coroutines
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Coroutines/PropertiesAndCoroutines.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertiesAndCoroutines : MonoBehaviour
+{
+    public float smoothing = 7f;
+    public Vector3 Target
+    {
+        get { return target; }
+        set
+        {
+            target = value;
+
+            StopCoroutine("Movement");
+            StartCoroutine("Movement", target);
+        }
+
+    }
+
+    private Vector3 target;
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Movement(Transform target)
+    {
+        while (Vector3.Distance(transform.position, target.position) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, smoothing * Time.deltaTime);
+
+            yield return null;
+        }
+}
