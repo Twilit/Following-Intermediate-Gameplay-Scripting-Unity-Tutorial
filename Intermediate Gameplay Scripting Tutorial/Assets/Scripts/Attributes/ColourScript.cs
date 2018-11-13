341936f2087308b7f2015e9dcfe35fb3d5ef17c7
Twilit <kinlongsha@hotmail.co.uk>
2018-11-13 11:42:47 +0000

    Attributes
    Finished

---
+++ Intermediate Gameplay Scripting Tutorial/Assets/Scripts/Attributes/ColourScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ColourScript : MonoBehaviour
+{
+	void Start ()
+    {
+        //renderer.sharedMaterial.color.red;
+	}
+}
